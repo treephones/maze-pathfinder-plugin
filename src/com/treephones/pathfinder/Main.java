eb2f6c4cad7e6a307805de5891e1baf326d56bc2
Moez B <moezbajwa@hotmail.com>
2020-11-28 15:01:53 -0500

    init

---
+++ src/com/treephones/pathfinder/Main.java
@@ -0,0 +1,14 @@
+package com.treephones.pathfinder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {}
+}
